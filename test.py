77034e238dfc6b69a9710e4a5a5ed4f762058066
Maciej Wilk <wilk.maciejcyryl@gmail.com>
2020-04-07 12:09:15 +0200

    started working on ply.lex lex to get strings from data

--- test.py
+++ test.py
@@ -1,11 +1,44 @@
 import build_in_functions
 import keywords
 import parser
+import sys
 
-st = parser.expr('a + 5')
-code = st.compile('file.py')
-a = 5
-eval(code)
+if ".." not in sys.path: sys.path.insert(0,"..")
+import pycparser.ply.lex as lex
 
 def create_token(start, end, name):
     pass
+
+
+tokens = (
+    'STRING',
+    'VAR'
+)
+
+def t_STRING(t):
+     r'"([^"\n]|(\\"))*"'
+     t.value = str(t.value)
+     return t
+
+def t_VAR(t):
+   r'[a-zA-Z_][a-zA-Z_0-9]*'
+   return t
+
+t_ignore = ' \t\r\n'
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = r'"nie" jest "Stringiem" nie jest varem'
+
+print("Data: '%s'" % data)
+
+lexer.input(data)
+
+while True:
+   tok = lexer.token()
+   if not tok: break
+   print(tok)
